738fde7e9d0bf51c4f0004549b7f48fb37e2e962
Avinash Iyer <avinashiyer4292@gmail.com>
2019-03-16 21:06:06 -0700

    constants added

--- app/src/main/java/com/example/aviyer/doordashsampleapp/network/UrlConfig.java
+++ app/src/main/java/com/example/aviyer/doordashsampleapp/network/UrlConfig.java
@@ -1,5 +1,8 @@
 package com.example.aviyer.doordashsampleapp.network;
 
+/**
+ * for storing constants
+ * */
 public class UrlConfig {
     public static final String DOORDASH_RESTAURANTS_BASE_URL="https://api.doordash.com/";
 }
