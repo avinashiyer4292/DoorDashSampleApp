120ebbc8d2060d597cf506bfcf225e49f549aaef
Avinash Iyer <avinashiyer4292@gmail.com>
2019-03-16 21:04:34 -0700

    Api service classes updated

--- app/src/main/java/com/example/aviyer/doordashsampleapp/network/Service.java
+++ app/src/main/java/com/example/aviyer/doordashsampleapp/network/Service.java
@@ -3,10 +3,18 @@ package com.example.aviyer.doordashsampleapp.network;
 import com.example.aviyer.doordashsampleapp.database.entities.Restaurant;
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 import java.util.List;
 
+/**
+ * Interface for performing calls and return response object
+ * */
+
 public interface Service {
-    @GET("/v2/restaurant/?lat=37.422740&lng=-122.139956")
-    Call<List<Restaurant>> getQuestions();
+    @GET("/v2/restaurant/")
+    Call<List<Restaurant>> getRestaurants(@Query("lat") double lat,
+                                          @Query("lng") double lng,
+                                          @Query("offset") int offset,
+                                          @Query("limit") int limit);
 }
--- app/src/main/java/com/example/aviyer/doordashsampleapp/network/ServiceGenerator.java
+++ app/src/main/java/com/example/aviyer/doordashsampleapp/network/ServiceGenerator.java
@@ -6,19 +6,30 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 import java.util.concurrent.TimeUnit;
 
+/**
+ * Service generator class which creates retrofit abd okhttp client objects to perform API request response
+ * */
 public class ServiceGenerator {
+    //createService (APIServce.class)
     public static <S> S createService(Class<S> serviceClass){
-        Retrofit.Builder builder = new Retrofit.Builder()
-                                    .addConverterFactory(GsonConverterFactory.create())
-                                    .baseUrl(UrlConfig.DOORDASH_RESTAURANTS_BASE_URL);
-        OkHttpClient.Builder httpClient = new OkHttpClient.Builder()
-                                            .readTimeout(90, TimeUnit.SECONDS)
-                                            .writeTimeout(90, TimeUnit.SECONDS)
-                                            .connectTimeout(90, TimeUnit.SECONDS)
-                                            .cache(null);
-
+        Retrofit.Builder builder = retrofitBuilder(UrlConfig.DOORDASH_RESTAURANTS_BASE_URL);
+        OkHttpClient.Builder httpClient = httpClientBuilder();
         builder.client(httpClient.build());
         Retrofit retrofit = builder.build();
         return retrofit.create(serviceClass);
+    }
+
+    private static Retrofit.Builder retrofitBuilder(String baseURL){
+        return new Retrofit.Builder()
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl(baseURL);
+    }
+
+    private static OkHttpClient.Builder httpClientBuilder(){
+        return new OkHttpClient.Builder()
+                .readTimeout(90, TimeUnit.SECONDS)
+                .writeTimeout(90, TimeUnit.SECONDS)
+                .connectTimeout(90, TimeUnit.SECONDS)
+                .cache(null);
     }
 }
