ab6218dcf2161149895b2a2cd4e96eca9914f5bc
Avinash Iyer <avinashiyer4292@gmail.com>
2019-03-16 21:09:23 -0700

    Update RestaurantUnitTest.java

--- app/src/test/java/com/example/aviyer/doordashsampleapp/RestaurantUnitTest.java
+++ app/src/test/java/com/example/aviyer/doordashsampleapp/RestaurantUnitTest.java
@@ -7,11 +7,6 @@ import org.mockito.Mock;
 
 import static org.junit.Assert.*;
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 public class RestaurantUnitTest {
 
     private Restaurant r;
