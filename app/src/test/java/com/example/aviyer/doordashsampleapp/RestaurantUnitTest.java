54cfee87ecd753fd624ba0bb970f9cd8bb0adab8
Avinash Iyer <avinashiyer4292@gmail.com>
2019-03-16 21:07:03 -0700

    Added restaurant unit test

--- app/src/test/java/com/example/aviyer/doordashsampleapp/RestaurantUnitTest.java
+++ app/src/test/java/com/example/aviyer/doordashsampleapp/RestaurantUnitTest.java
@@ -1,6 +1,9 @@
 package com.example.aviyer.doordashsampleapp;
 
+import com.example.aviyer.doordashsampleapp.database.entities.Restaurant;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
 
 import static org.junit.Assert.*;
 
@@ -9,9 +12,23 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-public class ExampleUnitTest {
+public class RestaurantUnitTest {
+
+    private Restaurant r;
+    @Before
+    public void setRestaurantObject(){
+        r = new Restaurant("Thirsty Bear Brewing Co","BrewPub",null,"20 mins",0.0f);
+    }
+
+
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void verifyIfNameNotEmpty() {
+        assertNotEquals(r.getName(), "");
+    }
+
+    @Test
+    public void verifyIfNameSetCorrectly(){
+        r.setName("Lee's Deli");
+        assertEquals(r.getName(), "Lee's Deli");
     }
 }
