1eef55145c5ae9b80f8ee9443a0069208fa2ca9a
Avinash Iyer <avinashiyer4292@gmail.com>
2019-03-16 21:03:28 -0700

    Refactor DAO class

--- app/src/main/java/com/example/aviyer/doordashsampleapp/database/dao/RestaurantsDao.java
+++ app/src/main/java/com/example/aviyer/doordashsampleapp/database/dao/RestaurantsDao.java
@@ -1,16 +1,28 @@
 package com.example.aviyer.doordashsampleapp.database.dao;
 
 import android.arch.lifecycle.LiveData;
-import android.arch.persistence.room.Dao;
-import android.arch.persistence.room.Query;
+import android.arch.persistence.room.*;
 import com.example.aviyer.doordashsampleapp.database.entities.Restaurant;
 
 import java.util.List;
+
+/**
+ * DAO class to access and perform database operations
+ * */
 
 @Dao
 public interface RestaurantsDao {
 
     @Query("Select * from restaurants")
     LiveData<List<Restaurant>> getAllRestaurants();
+
+    @Insert
+    void insert(Restaurant restaurant);
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertAllResturants(List<Restaurant> restaurants);
+
+    @Query("DELETE FROM restaurants")
+    void deleteAllRestaurants();
 
 }
